1c9e15549b8100a29fe65456194d5ccf5bd08c9c
KeroVieux <308680@gmail.com>
2018-10-23 19:33:49 +0800

    add package

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 require('../../src/css/screen.scss')
 require('../../src/css/sass/font-awesome/scss/font-awesome.scss')
+import { Observable } from 'rxjs'
+import _ from 'lodash'
+import Moment from 'moment'
 
 import h1 from '../templates/h1.hbs'
 document.write('<h1>Hello World111</h1>')
@@ -10,3 +13,13 @@ $(() => {
 const a = _.chunk([1, 2, 3], 2)
 console.log(a)
 console.log(Moment())
+const hello = Observable.create((observer) => {
+  _.forEach(_.times(3, String), (i) => {
+    setTimeout(() => {
+      observer.next(i)
+    }, 1000)
+  })
+})
+
+// 输出: 'Hello'...'World'
+hello.subscribe(val => {return console.log(val)})
