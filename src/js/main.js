eabd4a675e0912912d53f6690d494f91e88e62aa
KeroVieux <308680@gmail.com>
2018-10-24 14:30:05 +0800

    update demo

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 require('../../src/css/screen.scss')
 require('../../src/css/sass/font-awesome/scss/font-awesome.scss')
 import { Observable } from 'rxjs'
-import _ from 'lodash'
-import Moment from 'moment'
 
 import h1 from '../templates/h1.hbs'
 document.write('<h1>Hello World111</h1>')
@@ -14,11 +12,9 @@ const a = _.chunk([1, 2, 3], 2)
 console.log(a)
 console.log(Moment())
 const hello = Observable.create((observer) => {
-  _.forEach(_.times(3, String), (i) => {
-    setTimeout(() => {
-      observer.next(i)
-    }, 1000)
-  })
+  observer.next('Hello')
+  observer.next('World')
+  observer.next('!')
 })
 
 // 输出: 'Hello'...'World'
